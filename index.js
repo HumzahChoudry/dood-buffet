b01fd0bd2bd0a25efb2b8eb742ffa5a932756598
humzahchoudry <Humzah.choudry@gmail.com>
2020-04-25 15:34:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    setTimeout(function() {
+      document.querySelector('.eapps-remove-link').parentElement.remove()
+    }, 1000);
+
+    
+});
